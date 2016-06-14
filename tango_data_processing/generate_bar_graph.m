8e741d6febbc5b60503df3c0684b50244aec7a26
Robert Katzschmann <rkk@csail.mit.edu>
2016-06-14 15:42:52 -0400

    first iteration on bar graph

--- tango_data_processing/generate_bar_graph.m
+++ tango_data_processing/generate_bar_graph.m
@@ -45,65 +45,125 @@ containsBelt = strcmp(data(:,deviceCol),'belt') & containsUsefulData;
 [~,wallTapsCol,~] = find(strcmp(dataheader,'WallTaps'));
 [~,majCollCol,~] = find(strcmp(dataheader,'x_MajorCollision_boxOrIntoWall_'));
 
-datacompiled = struct();
-
-
+% set up the compiled data
+dc = struct();
 
 for ii = 1:4
 mazeName = ['mz' num2str(ii)];
 
+%CANE MAZE
+
 containsThisMaze = strcmp(data(:,taskCol),mazeName);
 [mzCaneRows,~,~] = find( containsCane & containsThisMaze);
-datacompiled.cane.maze{ii}.id = cell2mat(data(mzCaneRows,idCol));
-datacompiled.cane.maze{ii}.durationVideo = cell2mat(data(mzCaneRows,vidDurCol));
-datacompiled.cane.maze{ii}.durationTango = cell2mat(data(mzCaneRows,totTimCol));
+dc.cane.maze{ii}.id = cell2mat(data(mzCaneRows,idCol));
+dc.cane.maze{ii}.durationVideo.raw = cell2mat(data(mzCaneRows,vidDurCol));
+dc.cane.maze{ii}.durationTango.raw = cell2mat(data(mzCaneRows,totTimCol));
 
 figure(ii)
-plot(datacompiled.cane.maze{ii}.id,datacompiled.cane.maze{ii}.durationVideo,'*')
+plot(dc.cane.maze{ii}.id,dc.cane.maze{ii}.durationVideo.raw,'*')
 hold on
-plot(datacompiled.cane.maze{ii}.id,datacompiled.cane.maze{ii}.durationTango,'o')
+plot(dc.cane.maze{ii}.id,dc.cane.maze{ii}.durationTango.raw,'o')
 legend('duration video','duration tango');
 
 title(['cane ' ,mazeName])
-datacompiled.cane.maze{ii}.averageVelocity = cell2mat(data(mzCaneRows,aveVelCol));
-datacompiled.cane.maze{ii}.wallTaps = cell2mat(data(mzCaneRows,wallTapsCol));
+dc.cane.maze{ii}.averageVelocity.raw = cell2mat(data(mzCaneRows,aveVelCol));
+dc.cane.maze{ii}.wallTaps.raw = cell2mat(data(mzCaneRows,wallTapsCol));
+
+% Calculate averages and stds
+dc.cane.maze{ii}.durationVideo.mean = mean(dc.cane.maze{ii}.durationVideo.raw);
+dc.cane.maze{ii}.durationVideo.std = std(dc.cane.maze{ii}.durationVideo.raw);
+dc.cane.maze{ii}.durationTango.mean = mean(dc.cane.maze{ii}.durationTango.raw);
+dc.cane.maze{ii}.durationTango.std = std(dc.cane.maze{ii}.durationTango.raw);
+dc.cane.maze{ii}.wallTaps.mean = mean(dc.cane.maze{ii}.wallTaps.raw);
+dc.cane.maze{ii}.wallTaps.std = std(dc.cane.maze{ii}.wallTaps.raw);
 
 %BELT MAZE
 
 [mzBeltRows,~,~] = find( containsBelt & containsThisMaze);
-datacompiled.belt.maze{ii}.id = cell2mat(data(mzBeltRows,idCol));
+dc.belt.maze{ii}.id = cell2mat(data(mzBeltRows,idCol));
 
-datacompiled.belt.maze{ii}.durationVideo = cell2mat(data(mzBeltRows,vidDurCol));
-datacompiled.belt.maze{ii}.durationTango = cell2mat(data(mzBeltRows,totTimCol));
-datacompiled.belt.maze{ii}.averageVelocity = cell2mat(data(mzBeltRows,aveVelCol));
-datacompiled.belt.maze{ii}.majorColl = cell2mat(data(mzBeltRows,majCollCol));
+dc.belt.maze{ii}.durationVideo.raw = cell2mat(data(mzBeltRows,vidDurCol));
+dc.belt.maze{ii}.durationTango.raw = cell2mat(data(mzBeltRows,totTimCol));
+dc.belt.maze{ii}.averageVelocity.raw = cell2mat(data(mzBeltRows,aveVelCol));
+dc.belt.maze{ii}.majorColl.raw = cell2mat(data(mzBeltRows,majCollCol));
 
 figure(4+ii)
-plot(datacompiled.belt.maze{ii}.id,datacompiled.belt.maze{ii}.durationVideo,'*')
+plot(dc.belt.maze{ii}.id,dc.belt.maze{ii}.durationVideo.raw,'*')
 hold on
-plot(datacompiled.belt.maze{ii}.id,datacompiled.belt.maze{ii}.durationTango,'o')
+plot(dc.belt.maze{ii}.id,dc.belt.maze{ii}.durationTango.raw,'o')
 legend('duration video','duration tango');
 title(['belt ' ,mazeName])
 
+% Calculate averages and stds
+dc.belt.maze{ii}.durationVideo.mean = mean(dc.belt.maze{ii}.durationVideo.raw);
+dc.belt.maze{ii}.durationVideo.std = std(dc.belt.maze{ii}.durationVideo.raw);
+dc.belt.maze{ii}.durationTango.mean = mean(dc.belt.maze{ii}.durationTango.raw);
+dc.belt.maze{ii}.durationTango.std = std(dc.belt.maze{ii}.durationTango.raw);
+dc.belt.maze{ii}.majorColl.mean = mean(dc.belt.maze{ii}.majorColl.raw);
+dc.belt.maze{ii}.majorColl.std = std(dc.belt.maze{ii}.majorColl.raw);
+
+%BELT BOX
 
 boxName = ['bx' num2str(ii)];
 containsThisBox = strcmp(data(:,taskCol),boxName);
 
 [bxBeltRows,~,~] = find( containsBelt & containsThisBox);
-datacompiled.belt.box{ii}.durationVideo = data(bxBeltRows,vidDurCol);
-datacompiled.belt.box{ii}.durationTango = data(bxBeltRows,totTimCol);
-datacompiled.belt.box{ii}.averageVelocity = data(bxBeltRows,aveVelCol);
+dc.belt.box{ii}.durationVideo.raw = data(bxBeltRows,vidDurCol);
+dc.belt.box{ii}.durationTango.raw = data(bxBeltRows,totTimCol);
+dc.belt.box{ii}.averageVelocity.raw = data(bxBeltRows,aveVelCol);
 
-datacompiled.belt.maze{ii}.majorColl = data(mzCaneRows,majCollCol);
+dc.belt.box{ii}.majorColl.raw = data(mzCaneRows,majCollCol);
 
 end
 
+%% First Bar Graph
+figure(10)
+specs = {};
+numOfMazes = 4;
+for ii = 1:numOfMazes
+barh((numOfMazes-ii)*5+4,dc.belt.maze{ii}.durationVideo.mean,'FaceColor',hsv2rgb([0 1 1]),'EdgeColor',hsv2rgb([0 1 1]),'LineWidth',1.5);
+hold on;
+barh((numOfMazes-ii)*5+3,dc.belt.maze{ii}.majorColl.mean,'FaceColor',hsv2rgb([0.3 1 1]),'EdgeColor',hsv2rgb([0.3 1 1]),'LineWidth',1.5);
+barh((numOfMazes-ii)*5+2,dc.cane.maze{ii}.durationVideo.mean,'FaceColor',hsv2rgb([0 0.4 1]),'EdgeColor',hsv2rgb([0 0.4 1]),'LineWidth',1.5);
+barh((numOfMazes-ii)*5+1,dc.cane.maze{ii}.wallTaps.mean,'FaceColor',hsv2rgb([0.3 0.2 1]),'EdgeColor',hsv2rgb([0.3 0.2 1]),'LineWidth',1.5);
+specs = horzcat(specs,' ',['Belt Duration HW' num2str(ii)],['Belt Major Collisions HW' num2str(ii)],['Cane Duration HW' num2str(ii)],['Cane Wall Taps HW' num2str(ii)]);
 
-% bar(x,y)
-% hold on;
-% h=errorbar(x,y,e,'c'); set(h,'linestyle','none')
+%h=errorbar(x,y,e,'c'); set(h,'linestyle','none')
+end
+
+set(gca,'Ytick',1:numOfMazes*5+numOfMazes,'YTickLabel',specs(end:-1:1))
+
+
+%% Second Bar Graph
+figure(11)
+specs = {};
+numOfMazes = 4;
+for ii = 1:numOfMazes
+bar((ii-1)*5+1,dc.belt.maze{ii}.durationVideo.mean,'FaceColor',hsv2rgb([0 1 1.0]),'EdgeColor',hsv2rgb([0 1 1]),'LineWidth',1.5);
+hold on;
+errorbar((ii-1)*5+1,dc.belt.maze{ii}.durationVideo.mean,dc.belt.maze{ii}.durationVideo.std,'LineWidth',1.5,'Color',hsv2rgb([0 1 0.6]),'linestyle','none'); 
+
+bar((ii-1)*5+2,dc.belt.maze{ii}.majorColl.mean,'FaceColor',hsv2rgb([0.3 1 1]),'EdgeColor',hsv2rgb([0.3 1 1]),'LineWidth',1.5);
+errorbar((ii-1)*5+2,dc.belt.maze{ii}.majorColl.mean,dc.belt.maze{ii}.majorColl.std,'LineWidth',1.5,'Color',hsv2rgb([0.3 1 0.6]),'linestyle','none'); 
+
+bar((ii-1)*5+3,dc.cane.maze{ii}.durationVideo.mean,'FaceColor',hsv2rgb([0 0.4 1]),'EdgeColor',hsv2rgb([0 0.4 1]),'LineWidth',1.5);
+errorbar((ii-1)*5+3,dc.cane.maze{ii}.durationVideo.mean,dc.cane.maze{ii}.durationVideo.std,'LineWidth',1.5,'Color',hsv2rgb([0 1 0.6]),'linestyle','none'); 
+
+bar((ii-1)*5+4,dc.cane.maze{ii}.wallTaps.mean,'FaceColor',hsv2rgb([0.3 0.2 1]),'EdgeColor',hsv2rgb([0.3 0.2 1]),'LineWidth',1.5);
+errorbar((ii-1)*5+4,dc.cane.maze{ii}.wallTaps.mean,dc.cane.maze{ii}.wallTaps.std,'LineWidth',1.5,'Color',hsv2rgb([0.3 1 0.6]),'linestyle','none'); 
+
+specs = horzcat(specs,['Belt Duration HW' num2str(ii)],['Belt Major Collisions HW' num2str(ii)],['Cane Duration HW' num2str(ii)],['Cane Wall Taps HW' num2str(ii)],' ');
+
+ylim([-3 inf]) % or your lower limit.
+
+
+end
+
+set(gca,'Xtick',1:numOfMazes*5+numOfMazes,'XTickLabel',specs,'XTickLabelRotation',60)
+
+
 
 
 fileOutCompiledData = 'data-analysis-blind-users-20160524_with_tango_compiled.mat';
 filePathOutCompiledData = strcat(dropboxPath,fileOutCompiledData);
-save(filePathOutCompiledData,'datacompiled');
+save(filePathOutCompiledData,'dc');
